8a96796ec5bf3a437ac5d25c0aacbaebcb686bfb
SahariarIslm <dhruv.islam7584@gmail.com>
2026-09-09 11:44:28 +0600

    your squashed commit message

--- feature.py
+++ feature.py
@@ -4,3 +4,7 @@ url = "https://api.github.com/zen"
 req = urllib.request.Request(url, headers={"User-Agent": "Python"})
 with urllib.request.urlopen(req) as response:
     print(f"GitHub Zen: {response.read().decode('utf-8')}")
+    
+print('nothing 1')
+print('nothing 2')
+print('nothing 3')
